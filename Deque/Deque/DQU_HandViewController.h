9e9b6ab0ac9892c96c26b1da4d59b39e0bc46f45
xixia <snarkified.musings@gmail.com>
2014-04-06 22:07:01 -0400

    initial view implementation

---
+++ Deque/Deque/DQU_HandViewController.h
@@ -0,0 +1,13 @@
+//
+//  DQU_HandViewController.h
+//  Deque
+//
+//  Created by Xixia Wang on 4/6/14.
+//  Copyright (c) 2014 Xixia Wang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DQU_HandViewController : UICollectionViewController
+
+@end
